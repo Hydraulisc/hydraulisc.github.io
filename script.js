2bd4a05f4f462d40dee38f3729b30518e5f14c72
SleepingAmi <143962821+SleepingAmi@users.noreply.github.com>
2024-04-27 20:50:48 +0200

    Fix: Navigation + minor optimisation

--- script.js
+++ script.js
@@ -3,19 +3,8 @@ const closeMenu = document.querySelector(".close-menu");
 const menuWrapper = document.querySelector(".menu-wrapper");
 const hasCollapsible = document.querySelectorAll(".has-collapsible");
 const toggleAnimB = document.querySelector(".nav_anim_toggle_child");
-const drip1 = document.querySelector(".dripplets");
 
-window.onload = function() {
-    const dripCookie = getCookie("drip");
 
-    if(!dripCookie){
-        returnDrip();
-    } else if(dripCookie == "true") {
-        returnDrip();
-    } else if(dripCookie == "false") {
-        noDrip();
-    }
-}
 
 // simple link redirect funct for buttons
 function redirect(link) {
@@ -30,38 +19,6 @@ function getCookie(name) {
         return parts.pop().split(";").shift();
 }
 
-function noDrip() {
-    // Set the expiration date to one year from now
-    var expirationDate = new Date();
-    expirationDate.setFullYear(expirationDate.getFullYear() + 1);
-    // Set the cookie with the name "drip" and the desired value
-    document.cookie = "drip=" + false + "; expires=" + expirationDate.toUTCString() + "; SameSite=Strict";
-    drip1.style.display = "none";
-    toggleAnimB.innerHTML = `<img src="/assets/icons/drop.png" style="max-width: 32px;">`
-}
-
-function returnDrip() {
-    // Set the expiration date to one year from now
-    var expirationDate = new Date();
-    expirationDate.setFullYear(expirationDate.getFullYear() + 1);
-    // Set the cookie with the name "drip" and the desired value
-    document.cookie = "drip=" + true + "; expires=" + expirationDate.toUTCString() + "; SameSite=Strict";
-    drip1.style.display = "block";
-    toggleAnimB.innerHTML = `<img src="/assets/icons/nodrop.png" style="max-width: 32px;">`
-}
-
-// Navbar Drip Animation Toggle
-toggleAnimB.addEventListener("click", function(){
-    const dripCookie = getCookie("drip");
-
-    if(!dripCookie){
-        noDrip();
-    } else if(dripCookie == "true") {
-        noDrip();
-    } else if(dripCookie == "false") {
-        returnDrip();
-    }
-})
 
 // Sidenav Toggle
 openMenu.addEventListener("click", function () {
